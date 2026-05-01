89ecccec450bfba5a198f372c01256341580ee9f
medu-x <meduhammou@gmail.com>
2026-05-01 20:30:30 +0100

    Add temporary files list route

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,18 @@ use App\Http\Controllers\UploadController;
 
 
 
-
+use Illuminate\Support\Facades\Storage;
 Route::post('/upload', [UploadController::class, 'store']);
 
 
+Route::get('/files', function () {
+    try {
+        return response()->json([
+            'files' => Storage::files('uploads'),
+        ]);
+    } catch (\Exception $e) {
+        return response()->json([
+            'error' => $e->getMessage(),
+        ], 500);
+    }
+});
